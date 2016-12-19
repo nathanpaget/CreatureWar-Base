3f571107260634889b219d14c7150bc51294fa29
nathanpaget <npaget@comcast.net>
2016-12-19 15:46:08 -0500

    The war runs, and a winner is crowned

--- War.java
+++ War.java
@@ -28,30 +28,59 @@ public class War
         createArmy(armyOne);
         createArmy(armyTwo);
     }
-
+    
+    public void goToWar(){
+        while(armyOne.size()>0&&armyTwo.size()>0){
+            fight(armyOne.get(0),armyTwo.get(0));
+            if(!armyOne.get(0).isAlive()){
+                armyOne.remove(0);
+                System.out.println("A member of army 1 was defeated!");
+            }
+            if(!armyTwo.get(0).isAlive()){
+                armyTwo.remove(0);                
+                System.out.println("A member of army 2 was defeated!");
+            }
+        }
+        //System.out.println("Army 1 has "+ armyOne.size() +" members left");
+        //System.out.println("Army 2 has "+ armyTwo.size() +" members left");
+        if(armyOne.size()>armyTwo.size()){
+            System.out.println("Army 1 has won the battle");
+        }
+        else if(armyOne.size()<armyTwo.size()){
+            
+            System.out.println("Army 2 has won the battle");
+        }
+        else{
+            
+            System.out.println("In a fight to the bitter end, all souls perished");
+        }
+   }
+   private void fight(Creature c1, Creature c2){
+       int c1Damage=c1.damage();
+       int c2Damage=c2.damage();
+       c1.takeDamage(c2Damage);
+       c2.takeDamage(c1Damage);
+    }
+    
+    
     private void createArmy(ArrayList<Creature> army){
         int choice;
         for(int i=0;i<armySize;i++){
             choice=rand.nextInt(100);
             if(choice<20){
                 army.add(new Human());
-                System.out.println("add human");
             }
             else if(choice<40){
                 army.add(new Elf());
-                System.out.println("add elf");
             }
             else if(choice<90){
                 army.add(new CyberDemon());
-                System.out.println("add cyberdemon");
             }
             else if(choice<100){
                 army.add(new Balrog());
-                System.out.println("add balrog");
             }
             else{
                 army.add(new Human());
-                System.out.println("why are we here?");
             }
         }
     }
